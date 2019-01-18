56483ba7bdc0e5c4a67495b4764245d60113a00c
Arman2407 <martiros14@mail.ru>
2019-01-18 12:46:14 +0300

    Изменение main4

--- TaskJs4/main4.js
+++ TaskJs4/main4.js
@@ -6,18 +6,18 @@ function discriminant(a,b,c) {
   D = b**2 - 4*a*c;
 
   if (D < 0) {
-    console.log('Корней нет');
+    return'Корней нет';
   }
   else if (D == 0) {
     x1 = (-b) / (2*a);
-    console.log('x = ' + x1);
+    return'x = ' + x1;
   }
   else if (D > 0) {
     x1 = (-b + Math.sqrt(D)) / (2*a);
     x2 = (-b - Math.sqrt(D)) / (2*a);
-    console.log('x1 = ' + x1 + ' x2 = ' + x2);
+    return('x1 = ' + x1 + ' x2 = ' + x2);
   }
 
 }
 
-discriminant(2,4,-3);
+console.log(discriminant(2,4,-3));
